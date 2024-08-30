6843cc1788743af56506d22d9733c07d9ec454d2
macrocosme <d.vohl@uva.nl>
2024-08-30 14:24:53 +0200

    change prj to experiment

--- calibration.py
+++ calibration.py
@@ -1,16 +1,12 @@
 import os
 import numpy as np
-
 import funcs as _f
-
 
 basedir, workdir = os.path.split(os.path.abspath('.'))
 fitsdir='fits'
 calibdir='pipeline_calibration'
 
-set_vis = lambda prj, basedir=basedir, workdir=workdir: f'{basedir}/{workdir}/{prj}.ms'
-idifiles = _f.get_idifiles(basedir, fitsdir)
-
+set_vis = lambda experiment, basedir=basedir, workdir=workdir: f'{basedir}/{workdir}/{experiment}.ms'
 get_steps = lambda: ['unzip_gz',
                      'check_tsys_gaincurve',
                      'gen_list_of_scans'
@@ -21,18 +17,15 @@ get_steps = lambda: ['unzip_gz',
                      'apply_cal', 
                      'flag_autocorrelation', 
                      'flagquack_intervals'] 
+dict_as_list = lambda _dict: (_dict[v] for v in _dict.keys())
 
-
-def dict_as_list(_dict):
-    return (_dict[v] for v in _dict.keys())
-
-def get_variables(prj, refant = 'EF', return_as_dict=False):
+def get_variables(experiment, refant = 'EF', return_as_dict=False):
     """Get values for common variables
 
     Parameters
     ----------
-        prj: str
-            prj code
+        experiment: str
+            experiment code
         refant: str
             Reference antenna (default: 'EF')
         return_as_dict: bool
@@ -43,8 +36,9 @@ def get_variables(prj, refant = 'EF', return_as_dict=False):
         dict: vis, refant, gcaltab, tsystab, sbdtab, mbdtab, bpasstab, idifiles
     """
     refant = refant
-    vis = set_vis(prj)
-    gcaltab, tsystab, sbdtab, mbdtab, bpasstab = _f.set_working_vars(basedir, workdir, prj)
+    vis = set_vis(experiment)
+    gcaltab, tsystab, sbdtab, mbdtab, bpasstab = _f.set_working_vars(basedir, workdir, experiment)
+    idifiles = _f.get_idifiles(basedir, fitsdir, experiment)
     
     d = {'vis': vis,
          'refant': refant, 
@@ -60,38 +54,39 @@ def get_variables(prj, refant = 'EF', return_as_dict=False):
     else: 
         return dict_as_list(d)
 
-def run_steps(prj, 
+def run_steps(experiment, 
               steps=get_steps()):
     """Generate strings matching function calls
 
     Parameters
     ----------
-        prj, steps
+        experiment, steps
 
     """
+    idifiles = _f.get_idifiles(basedir, fitsdir, experiment)
     is_in_steps = lambda step, steps=steps: (step in steps)
     vis, \
         refant, \
         gcaltab, tsystab, sbdtab, mbdtab, bpasstab, \
-        idifiles = get_variables(prj)
+        idifiles = get_variables(experiment)
 
     if is_in_steps('unzip_gz'):
         _f.gunzip(basedir, calibdir)
 
     if is_in_steps('import_fits_idi'):
-        _f.import_fits_idi(basedir, fitsdir, workdir, prj, vis, idifiles)
+        _f.import_fits_idi(basedir, fitsdir, workdir, experiment, vis, idifiles)
 
-    if is_in_steps('check_tsys_gaincurve'):
-        _f.append_tsys_gaincurve(basedir, calibdir, prj, idifiles)
+    if is_in_steps('check_tsys_gaincurve'):  
+        _f.append_tsys_gaincurve(basedir, calibdir, experiment, idifiles)
 
     if is_in_steps('gen_list_of_scans'):
-        _f.gen_list_of_scans(basedir, calibdir, prj, vis)
+        _f.gen_list_of_scans(basedir, calibdir, experiment, vis)
 
     if is_in_steps('flag_data'):
-        _f.flag_data(basedir, workdir, prj, vis)
+        _f.flag_data(basedir, workdir, experiment, vis)
 
     if is_in_steps('convert_flag'):
-        _f.convert_flag(basedir, calibdir, workdir, prj, idifiles)
+        _f.convert_flag(basedir, calibdir, workdir, experiment, idifiles)
 
     if is_in_steps('gen_cal'):
         _f.gen_cal(vis, tsystab, gcaltab, vis)
@@ -116,7 +111,7 @@ def run_steps(prj,
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('prj', nargs='+', type=str,
+    parser.add_argument('experiment', nargs='+', type=str,
                         help='project id (in lower case)')
     parser.add_argument("-s", "--steps", 
                         nargs='+', 
@@ -125,7 +120,7 @@ if __name__ == '__main__':
                             help="Steps to be run")
 
     args = parser.parse_args()
-    run_steps(args.prj, args.steps)
+    run_steps(args.experiment, args.steps)
 
 
 # FRINGE FITTING
